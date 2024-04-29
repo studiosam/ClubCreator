f5afdc4690bd439b038c5b68583361a9febf366d
sam <studiosam423@gmail.com>
2024-04-28 21:32:33 -0400

    2024.04.28.21:33

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const app = express();
 const PORT = 3000;
 
 // Array to store club suggestions from teachers
-let clubSuggestionsFromTeachers = [];
+let clubsThatNeedCoSponsors = [];
+let clubsToBeApproved = [];
 
 // Middleware to parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // CORS middleware for allowing cross-origin requests
 app.use(cors());
+
 
 // Serve the index.html file for the root URL
 app.get("/", (req, res) => {
@@ -21,6 +24,11 @@ app.get("/", (req, res) => {
 // Serve the clubCreation.html file
 app.get("/clubCreation.html", (req, res) => {
   res.sendFile(path.join(__dirname, "clubCreation.html")); // Make sure the path matches your file structure
+});
+
+// API endpoint to get the list of clubs
+app.get("/getClubsThatNeedCoSponsors", (req, res) => {
+  res.json(clubsThatNeedCoSponsors); // Sends the array of clubs as JSON
 });
 
 // POST route to handle form submission from clubCreation.html
@@ -34,8 +42,8 @@ app.post("/submit", (req, res) => {
     !teacherFirstName ||
     !teacherLastName ||
     !preferredClub ||
-    !coSponsorsNeeded ||
-    !maxCapacity
+    !Number.isInteger(coSponsorsNeeded) ||
+    !Number.isInteger(maxCapacity)
   ) {
     return res.status(400).send("All fields are required.");
   }
@@ -48,8 +56,10 @@ app.post("/submit", (req, res) => {
     maxCapacity
   };
 
-  clubSuggestionsFromTeachers.push(newClub);
-  console.log(clubSuggestionsFromTeachers);
+  if (newClub.coSponsorsNeeded > 0) {
+    clubsThatNeedCoSponsors.push(newClub);
+  }
+  clubsToBeApproved.push(newClub);
 
   res.send("Club suggestion submitted successfully!");
 });
