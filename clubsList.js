8754cff79436ec39d887f411b455efc4d31c8620
sam <studiosam423@gmail.com>
2024-11-04 17:46:08 -0500

    1.0.0
    Co-authored-by: Chris <chrisdogtn@users.noreply.github.com>

---
+++ clubsList.js
@@ -0,0 +1,46 @@
+const clubsDiv = document.querySelector('#clubs')
+const printbtn = document.querySelectorAll('.printbtn');
+
+printbtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        let divContents = clubsDiv.innerHTML;
+        if (divContents !== '') {
+            let a = window.open('', '');
+            a.document.write('<html>');
+            a.document.write('<body>');
+            a.document.write(divContents);
+            a.document.write('</body></html>');
+            a.document.close();
+            a.print();
+        }
+    })
+})
+
+async function getClubsList() {
+    const allClubs = await fetch(`http://${serverAddress}:3000/getAllClubs`);
+    const allClubsJson = await allClubs.json();
+    allClubsJson.sort((a, b) => {
+        if (a.clubName < b.clubName) {
+            return -1;
+        }
+        if (a.clubName > b.clubName) {
+            return 1;
+        }
+        return 0;
+    });
+    allClubsJson.forEach(async (club) => {
+        const teacherresponse = await fetch(`http://${serverAddress}:3000/usersInfo/${club.primaryTeacherId}`)
+        const teacher = await teacherresponse.json()
+        const response = await fetch(`http://${serverAddress}:3000/getClubById?club=${club.clubId}`)
+        const clubJson = await response.json();
+        const clubName = clubJson.clubName
+        clubsDiv.innerHTML += `<div><a href="http://${serverAddress}/club-info.html?club-id=${clubJson.clubId}"><span>${clubName}</span></a><span> | ${teacher.firstName} ${teacher.lastName}</span><span> | Room: ${club.room}</span></div>`
+
+    });
+
+}
+
+
+
+
+getClubsList() 
